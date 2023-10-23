d7e6b262b9e973d8461e9b41fa7b12e8627e7e7a
Nikola <niuku27@gmail.com>
2023-10-23 21:22:38 +0200

    ITP-150
    Style the header text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import SignUpCard from "@/components/SignUpCard";
+import SignUpCard from '@/components/SignUpCard';
 
 export default function Home() {
   return (
     <div>
-      <h1>Quiz yourself on your chosen topic with IntelliQ</h1>
-      <h3>
-        I am IntelliQ, created and maintained by <span>ARC-Solutions</span>
+      <h1 className='grid grid-flow-col place-content-center text-3xl'>
+        Quiz yourself on your chosen topic with 
+        <span className='text-primary ml-1'>IntelliQ</span>.
+      </h1>
+
+      <h3 className='grid grid-flow-col place-content-center'>
+        I am IntelliQ, created and maintained by
+        <span>ARC-Solutions</span>
       </h3>
-      <SignUpCard />
+
+      <div className='grid place-content-center'>
+        <SignUpCard />
+      </div>
     </div>
   );
 }
