ad52d7ba045941052c42f28b6c815a821a9d22fe
Nippon <nippon.lama@gmail.com>
2023-10-23 22:57:23 +0200

    ITP-184 refactor

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { IoExitOutline } from "react-icons/io5";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar} from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
