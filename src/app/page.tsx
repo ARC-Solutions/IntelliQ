f84e5c7197c1a6aae79825a514e1e584f130d31f
Nikola <niuku27@gmail.com>
2023-10-28 20:49:17 +0200

    Current State of Style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import SignUpCard from "@/components/SignUpCard";
+import SignUpCard from '@/components/SignUpCard';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Quiz yourself on your chosen topic with IntelliQ</h1>
-      <h3>
-        I am IntelliQ, created and maintained by <span>ARC-Solutions</span>
-      </h3>
-      <SignUpCard />
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className='text-3xl text-center'>
+        Quiz yourself on your chosen topic with
+        <span className='text-primary'> IntelliQ</span>.
+      </h1>
+      <div className='text-[17.5px] font-light opacity-70 m-2 mb-5 text-center'>
+        I am IntelliQ, created and maintained by
+        <span className='text-primary'> ARC-Solutions</span>
+      </div>
+      <div className='flex items-center justify-center'>
+        <SignUpCard />
+      </div>
     </div>
   );
 }
--- src/components/SignUpCard.tsx
+++ src/components/SignUpCard.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -8,34 +8,30 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useAuth } from "../contexts/UserContext";
-import { useEffect, useRef, useState } from "react";
-import { FcGoogle } from "react-icons/fc";
-import { toast } from "react-toastify";
-import { redirect } from "next/navigation";
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useAuth } from '../contexts/UserContext';
+import { useEffect, useRef, useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { redirect } from 'next/navigation';
 const SignUpCard = () => {
   const [isANewUser, setIsAnewUser] = useState(false);
-  const { currentUser, setCurrentUser, signinUsingEmail, signupUsingEmail } =
-    useAuth();
+  const { currentUser, setCurrentUser, signinUsingEmail, signupUsingEmail } = useAuth();
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const confirmPasswordRef = useRef<HTMLInputElement>(null);
   const handleSubmit = async (isGoogleOAuth: boolean = false) => {
     if (isGoogleOAuth) {
       try {
-        const response = await fetch(
-          "https://intelliq-be.azurewebsites.net/api/signin",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ provider: "google" }),
-          }
-        );
+        const response = await fetch('https://intelliq-be.azurewebsites.net/api/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ provider: 'google' }),
+        });
         const data = await response.json();
 
         if (data.url) {
@@ -43,7 +39,7 @@ const SignUpCard = () => {
         }
         return;
       } catch (error) {
-        toast.error("Failed to initiate Google sign in.");
+        toast.error('Failed to initiate Google sign in.');
         return;
       }
     }
@@ -53,14 +49,14 @@ const SignUpCard = () => {
     const confirmPassword = confirmPasswordRef.current?.value as string;
 
     if (!email || !password || (isANewUser && !confirmPassword)) {
-      toast.error("Please fill out all the fields");
+      toast.error('Please fill out all the fields');
       return;
     }
 
     if (isANewUser) {
       confirmPassword === password
         ? signupUsingEmail({ email, password })
-        : toast.error("Your Password does not match");
+        : toast.error('Your Password does not match');
     } else {
       signinUsingEmail({ email, password });
     }
@@ -70,7 +66,7 @@ const SignUpCard = () => {
     const pattern = /-auth-token$/;
     const localStorageKeys = Object.keys(localStorage);
     const authTokens = localStorageKeys.filter((key) => pattern.test(key))[0];
-    if (typeof window !== "undefined") {
+    if (typeof window !== 'undefined') {
       const storedItem = localStorage.getItem(authTokens);
       if (storedItem !== null) {
         const {
@@ -87,64 +83,77 @@ const SignUpCard = () => {
 
   useEffect(() => {
     if (currentUser) {
-      redirect("/dashboard");
+      redirect('/dashboard');
     }
   }, [currentUser]);
   return (
-    <Card className="w-[500px]">
-      <CardHeader>
-        <CardTitle>ARC-Solutions</CardTitle>
-        <CardDescription>Let&apos;s Sign You In</CardDescription>
-        <Button onClick={() => handleSubmit(true)}>
-          <FcGoogle size="" />
+    <Card className='w-full md:w-[450px] border border-primary rounded-lg shadow-md p-4'>
+      <CardHeader className='pb-5'>
+        <CardTitle className='text-2xl'>ARC-Solutions</CardTitle>
+        <CardDescription className='pt-1 pb-2'>Let&apos;s Sign You In</CardDescription>
+        <Button onClick={() => handleSubmit(true)} className='bg-[#fafafa] hover:bg-[#d5d5d5] p-5'>
+          <div className='text-3xl place-content-center'>
+            <FcGoogle size='' />
+          </div>
         </Button>
+        <div className='border-b-[0.5px] border-white pt-1 pb-1 border-opacity-60'></div>
       </CardHeader>
       <CardContent>
         <form>
           <div>
-            <div>
-              <Label htmlFor="email">Email address</Label>
-              <Input
-                type="email"
-                id="email"
-                placeholder="Enter you email address"
-                ref={emailRef}
-              />
+            <div className='mb-5'>
+              <div className='mb-1.5'>
+                <Label htmlFor='email' className='font-thin'>
+                  Email address
+                </Label>
+              </div>
+              <Input type='email' id='email' placeholder='Enter your Email' ref={emailRef} />
             </div>
-            <div>
-              <Label htmlFor="password">Your Password</Label>
+            <div className='mb-5'>
+              <div className='mb-1.5'>
+                <Label htmlFor='password' className='font-thin'>
+                  Your Password
+                </Label>
+              </div>
               <Input
-                id="password"
-                type="password"
-                placeholder="Enter your Password"
+                id='password'
+                type='password'
+                placeholder='Enter your Password'
                 ref={passwordRef}
               />
             </div>
             {isANewUser && (
-              <div>
-                <Label htmlFor="confirmPassword">Confirm Password</Label>
+              <div className='mb-4'>
+                <div className='mb-1.5'>
+                  <Label htmlFor='confirmPassword' className='font-thin'>
+                    Confirm Password
+                  </Label>
+                </div>
                 <Input
-                  id="confirmPassword"
-                  type="password"
-                  placeholder="Confirm your Password"
+                  id='confirmPassword'
+                  type='password'
+                  placeholder='Confirm your Password'
                   ref={confirmPasswordRef}
                 />
               </div>
             )}
           </div>
         </form>
-        <Button onClick={() => handleSubmit(false)}>
-          {isANewUser ? "Sign Up" : "Sign In"}
+        <Button
+          onClick={() => handleSubmit(false)}
+          className='w-full text-center font-[550] rounded-none text-xl p-5'
+        >
+          {isANewUser ? 'Sign Up' : 'Sign In'}
         </Button>
       </CardContent>
       <CardFooter>
         <h3>
-          Already have an account?{" "}
+          Already have an account?{' '}
           <span
-            className="cursor-pointer"
+            className='cursor-pointer text-primary underline hover:text-primary/90'
             onClick={() => setIsAnewUser(!isANewUser)}
           >
-            {isANewUser ? "Sign In" : "Sign Up"}
+            {isANewUser ? 'Sign In' : 'Sign Up'}
           </span>
         </h3>
       </CardFooter>
