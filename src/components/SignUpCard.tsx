43646a95dca4da809bac615fea8a4b77dd263458
Nippon <nippon.lama@gmail.com>
2023-10-31 17:17:55 +0100

    ITP-189 feat

--- src/components/SignUpCard.tsx
+++ src/components/SignUpCard.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,13 +8,13 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { useAuth } from '../contexts/UserContext';
-import { useRef, useState } from 'react';
-import { FcGoogle } from 'react-icons/fc';
-import { toast } from 'react-toastify';
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useAuth } from "../contexts/UserContext";
+import { useRef, useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
 const SignUpCard = () => {
   const [isANewUser, setIsAnewUser] = useState(false);
   const { signinUsingEmail, signupUsingEmail, signinUsingOAuth } = useAuth();
@@ -27,7 +27,7 @@ const SignUpCard = () => {
         signinUsingOAuth();
         return;
       } catch (error) {
-        toast.error('Failed to initiate Google sign in.');
+        toast.error("Failed to initiate Google sign in.");
         return;
       }
     }
@@ -37,97 +37,102 @@ const SignUpCard = () => {
     const confirmPassword = confirmPasswordRef.current?.value as string;
 
     if (!email || !password || (isANewUser && !confirmPassword)) {
-      toast.error('Please fill out all the fields');
+      toast.error("Please fill out all the fields");
       return;
     }
 
     if (isANewUser) {
       confirmPassword === password
         ? signupUsingEmail({ email, password })
-        : toast.error('Your Password does not match');
+        : toast.error("Your Password does not match");
     } else {
       signinUsingEmail({ email, password });
     }
   };
   return (
-    <Card className='w-full sm:w-[450px] border-none rounded-lg p-4 shadow-[0_0px_20px_-4px_rgba(0,0,0,0.24)] shadow-primary'>
-      <CardHeader className='pb-5'>
-        <CardTitle className='text-3xl'>ARC-Solutions</CardTitle>
-        <CardDescription className='pt-1 pb-2 text-xl'>Let&apos;s Sign You In</CardDescription>
+    <Card className="w-full sm:w-[450px] border-none rounded-lg p-4 shadow-[0_0px_20px_-4px_rgba(0,0,0,0.24)] shadow-primary">
+      <CardHeader className="pb-5">
+        <CardTitle className="text-3xl">ARC-Solutions</CardTitle>
+        <CardDescription className="pt-1 pb-2 text-xl">
+          Let&apos;s Sign You In
+        </CardDescription>
         <Button
           onClick={() => handleSubmit(true)}
-          className='bg-[#fafafa] hover:bg-[#fafafa]/90 p-5'
+          className="bg-[#fafafa] hover:bg-[#fafafa]/90 p-5"
         >
-          <div className='text-3xl place-content-center'>
-            <FcGoogle size='' />
+          <div className="text-3xl place-content-center">
+            <FcGoogle size="" />
           </div>
         </Button>
-        <div className='border-b-[0.5px] border-white pt-1 pb-1 border-opacity-70'></div>
+        <div className="border-b-[0.5px] border-white pt-1 pb-1 border-opacity-70"></div>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit(false);
+        }}>
           <div>
-            <div className='mb-5'>
-              <div className='mb-1.5'>
-                <Label htmlFor='email' className='font-thin'>
+            <div className="mb-5">
+              <div className="mb-1.5">
+                <Label htmlFor="email" className="font-thin">
                   Email Address
                 </Label>
               </div>
               <Input
-                type='email'
-                id='email'
-                placeholder='Enter your Email Address'
+                type="email"
+                id="email"
+                placeholder="Enter your Email Address"
                 ref={emailRef}
-                className='border-2'
+                className="border-2"
               />
             </div>
-            <div className='mb-5'>
-              <div className='mb-1.5'>
-                <Label htmlFor='password' className='font-thin'>
+            <div className="mb-5">
+              <div className="mb-1.5">
+                <Label htmlFor="password" className="font-thin">
                   Your Password
                 </Label>
               </div>
               <Input
-                id='password'
-                type='password'
-                placeholder='Enter your Password'
+                id="password"
+                type="password"
+                placeholder="Enter your Password"
                 ref={passwordRef}
-                className='border-2'
+                className="border-2"
               />
             </div>
             {isANewUser && (
-              <div className='mb-4'>
-                <div className='mb-1.5'>
-                  <Label htmlFor='confirmPassword' className='font-thin'>
+              <div className="mb-4">
+                <div className="mb-1.5">
+                  <Label htmlFor="confirmPassword" className="font-thin">
                     Confirm Password
                   </Label>
                 </div>
                 <Input
-                  id='confirmPassword'
-                  type='password'
-                  placeholder='Confirm your Password'
+                  id="confirmPassword"
+                  type="password"
+                  placeholder="Confirm your Password"
                   ref={confirmPasswordRef}
-                  className='border-2'
+                  className="border-2"
                 />
               </div>
             )}
           </div>
+          <Button
+            type="submit"
+            className="w-full text-center font-[550] rounded-none text-xl p-5"
+          >
+            {isANewUser ? "Sign Up" : "Sign In"}
+          </Button>
         </form>
-        <Button
-          onClick={() => handleSubmit(false)}
-          className='w-full text-center font-[550] rounded-none text-xl p-5'
-        >
-          {isANewUser ? 'Sign Up' : 'Sign In'}
-        </Button>
       </CardContent>
-      <CardFooter className='place-content-center font-extralight opacity-70'>
+      <CardFooter className="place-content-center font-extralight opacity-70">
         <h3>
-          Already have an account?{' '}
+          Already have an account?{" "}
           <span
-            className='cursor-pointer text-primary underline hover:text-primary/90'
+            className="cursor-pointer text-primary underline hover:text-primary/90"
             onClick={() => setIsAnewUser(!isANewUser)}
           >
-            {isANewUser ? 'Sign In' : 'Sign Up'}
+            {isANewUser ? "Sign In" : "Sign Up"}
           </span>
         </h3>
       </CardFooter>
