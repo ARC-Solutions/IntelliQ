47a197b53917cd148bb5d1736aa3b72b430e535a
Nikola <niuku27@gmail.com>
2023-11-28 11:12:54 +0100

    selected answer
    Add bg-primary to the selected answer for each question, the bg is changed based on what answer the user presses

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -12,11 +12,11 @@ const Answer = ({ answer, letter }: Props) => {
   return (
     <Button
       onClick={() => dispatch({ type: 'SET_SELECTED_ANSWER', payload: answer })}
-      className='group w-full my-3 p-7 text-sm sm:text-xl bg-black text-white border rounded-lg border-white border-opacity-30 justify-start hover:text-black hover:border-none font-medium'
+      className='group w-full my-3 p-7 text-sm sm:text-xl bg-black text-white border rounded-lg border-white border-opacity-30 justify-start hover:text-black hover:border-none font-medium focus:bg-primary focus:text-black'
     >
       <span
         id='letter'
-        className='me-4 text-base sm:text-2xl border border-white px-2 py-1 border-opacity-[.15] rounded-md group-hover:border-black '
+        className='me-4 text-base sm:text-2xl border border-white px-2 py-1 border-opacity-20 rounded-md group-hover:border-black group-focus:border-black'
       >
         {letter}
       </span>
