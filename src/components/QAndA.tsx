8177d2dff91c8f987955812ea8706a67c6e7d205
Nikola <niuku27@gmail.com>
2023-12-03 19:34:28 +0100

    ITP-263
    Style the summary page according to the mockup, with some styling that is not on the mockup such as striped table.

--- src/components/QAndA.tsx
+++ src/components/QAndA.tsx
@@ -11,8 +11,8 @@ const QAndA = ({ quiz, questionNumber }: Props) => {
   const questionsAndAnswers = quiz[questionNumber] as Quiz;
   let { options: answers, text: question } = questionsAndAnswers;
   return (
-    <section className='items-center justify-center'>
-      <h1 className='bg-primary rounded-md text-base sm:text-2xl text-black p-6 font-bold w-full text-center'>{question}</h1>
+    <section>
+      <h1 className='bg-primary rounded-md text-base sm:text-2xl text-black p-6 font-bold w-full text-center items-center'>{question}</h1>
       <div className='w-auto mt-4'>
         {answers.map((answer, i) => {
           return <Answer key={i} answer={answer.slice(3)} letter={answer.substring(0, 3)}></Answer>;
