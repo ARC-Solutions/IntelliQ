d74ab366292bb375428d0e580b80d96610b75108
RickyRAV <rickyraveanu02@gmail.com>
2023-12-20 00:15:23 +0100

    hotfix: itp-281

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,5 +1,18 @@
+import {Skeleton} from "@/components/ui/skeleton";
+
 const loading = () => {
-  return <div>loading</div>;
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            <Skeleton className='flex h-9 w-48 mb-5 rounded-full'/>
+            <div>
+                <Skeleton className="flex h-[170px] w-[350px] mb-20 rounded-md"/>
+            </div>
+            <div className="space-y-5">
+                <Skeleton className="h-[424px] w-[350px] rounded-lg"/>
+                <Skeleton className="h-[430px] w-[350px] rounded-lg"/>
+            </div>
+        </div>
+    );
 };
 
 export default loading;
--- src/app/quiz/loading.tsx
+++ src/app/quiz/loading.tsx
@@ -1,9 +1,28 @@
-
+import { Skeleton } from "@/components/ui/skeleton"
 
 const loading = () => {
   return (
-    <div>loading</div>
-  )
+      <div className='flex flex-col items-center justify-center'>
+        <Skeleton className='flex h-9 w-48 mb-5 rounded-full'/>
+        <div className='flex justify-between items-center mb-4'>
+          <Skeleton className="flex h-[36px] w-[130px] rounded-md"/>
+          <Skeleton className="flex h-[42px] w-[150px] rounded-md"/>
+        </div>
+        <div>
+            <Skeleton className="h-[24px] w-[300px] mb-20 rounded-md"/>
+        </div>
+        <div className="w-full flex flex-col items-center justify-center">
+          <Skeleton className="h-[80px] w-[720px] rounded-lg mb-10"/>
+          <div className='w-auto'>
+            <Skeleton className="h-[58px] w-[720px] rounded-lg mb-5"/>
+            <Skeleton className="h-[58px] w-[720px] rounded-lg mb-5"/>
+            <Skeleton className="h-[58px] w-[720px] rounded-lg mb-5"/>
+            <Skeleton className="h-[58px] w-[720px] rounded-lg mb-5"/>
+          </div>
+          <Skeleton className="h-[36px] w-[100px] rounded-lg"/>
+        </div>
+      </div>
+  );
 }
 
 export default loading
