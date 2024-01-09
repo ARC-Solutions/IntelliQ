283f029fa3247a22f6b09cc51e3925a97ecae0b4
Nikola <niuku27@gmail.com>
2024-01-09 18:55:57 +0100

    ITP-282
    Base is finished

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,61 +1,69 @@
-"use client";
-import QuizMe from "@/components/QuizMe";
-import TopPicks from "@/components/TopPicks";
-import {QuizHistories, useQuiz} from "@/contexts/QuizContext";
-import LoadingQuestions from "./LoadingQuestions";
-import {useEffect} from "react";
-import {redirect} from "next/navigation";
-import QuizHistory from "./QuizHistory";
+'use client';
+import QuizMe from '@/components/QuizMe';
+import TopPicks from '@/components/TopPicks';
+import { QuizHistories, useQuiz } from '@/contexts/QuizContext';
+import LoadingQuestions from './LoadingQuestions';
+import { useEffect } from 'react';
+import { redirect } from 'next/navigation';
+import QuizHistory from './QuizHistory';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { FaQuestionCircle } from 'react-icons/fa';
+import Modal from '@/components/Modal';
 
 const Dashboard = ({
-                       prevQuizzes,
-                   }: {
-    prevQuizzes: {
-        quizzes: QuizHistories[],
-        totalCount: number
-    }
+  prevQuizzes,
+}: {
+  prevQuizzes: {
+    quizzes: QuizHistories[];
+    totalCount: number;
+  };
 }) => {
-    const {
-        isLoading,
-        fetchingFinished: finished,
-        currentQuiz,
-        dispatch,
-        summaryQuiz
-    } = useQuiz();
-    useEffect(() => {
-        dispatch({type: "RESET_SUMMARY_QUIZ"});
-        dispatch({type: "STORE_QUIZZES", payload: prevQuizzes.quizzes});
-    }, []);
-    useEffect(() => {
-        if (currentQuiz) {
-            const url = `/quiz/play`;
-            redirect(url);
-        }
-        if (summaryQuiz) {
-            redirect(`/summary/${summaryQuiz.quiz_id}`);
-        }
-    }, [currentQuiz, summaryQuiz]);
-
-
-    if (isLoading) {
-        return <LoadingQuestions finished={finished}/>;
+  const { isLoading, fetchingFinished: finished, currentQuiz, dispatch, summaryQuiz } = useQuiz();
+  useEffect(() => {
+    dispatch({ type: 'RESET_SUMMARY_QUIZ' });
+    dispatch({ type: 'STORE_QUIZZES', payload: prevQuizzes.quizzes });
+  }, []);
+  useEffect(() => {
+    if (currentQuiz) {
+      const url = `/quiz/play`;
+      redirect(url);
     }
-    return (
-      <div className='flex flex-col items-center justify-center'>
-        <h1 className='text-3xl lg:text-4xl text-center font-semibold'>DASHBOARD</h1>
-        <div className='lg:grid lg:grid-flow-col gap-8 mt-12 m-2'>
-          <div className='col-span-1 m-4 lg:m-0'>
-            <QuizMe />
-          </div>
-          <div className='row-span-2 mt-10 col-span-1 m-4 lg:m-0'>
-            <TopPicks />
-          </div>
-          <div className='row-span-3 mt-10 lg:mt-0 m-4 lg:m-0'>
-            <QuizHistory totalQuiz={prevQuizzes.totalCount} />
-          </div>
+    if (summaryQuiz) {
+      redirect(`/summary/${summaryQuiz.quiz_id}`);
+    }
+  }, [currentQuiz, summaryQuiz]);
+
+  if (isLoading) {
+    return <LoadingQuestions finished={finished} />;
+  }
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className='text-3xl lg:text-4xl text-center font-semibold'>DASHBOARD</h1>
+      <Link href={'/modal'} className='p-0 m-0'>
+        <div className='flex justify-center items-center p-1'>
+          <Button className='w-full text-sm bg-[#c8b6ff] rounded-xl'>
+            <div className='flex items-center justify-center'>
+              <p className='font-bold mr-2 '>What is IntelliQ</p>
+              <FaQuestionCircle />
+            </div>
+          </Button>
+        </div>
+      </Link>
+      <Modal />
+      <div className='lg:grid lg:grid-flow-col gap-8 mt-12 m-2'>
+        <div className='col-span-1 m-4 lg:m-0'>
+          <QuizMe />
+        </div>
+        <div className='row-span-2 mt-10 col-span-1 m-4 lg:m-0'>
+          <TopPicks />
+        </div>
+        <div className='row-span-3 mt-10 lg:mt-0 m-4 lg:m-0'>
+          <QuizHistory totalQuiz={prevQuizzes.totalCount} />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/Modal.tsx
@@ -0,0 +1,80 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { FaGithub } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { SiTailwindcss } from 'react-icons/si';
+const Modal = () => {
+  return (
+    <div className='flex items-center justify-center h-[76.5vh]'>
+      <Card className='w-full md:w-auto lg:w-[970px] md:h-[410px] border-primary rounded-none p-4 pb-10 m-4 shadow-[0_0px_18px_-4px_rgba(0,0,0,0.24)] shadow-primary'>
+        <CardHeader className='pb-3 pt-0'>
+          <CardTitle className='text-2xl sm:text-4xl font-bold pb-3'>
+            Welcome to IntelliQ!
+          </CardTitle>
+          <div className='flex items-center'>
+            <FaGithub className='w-6 h-6 sm:w-7 sm:h-7 mr-2' />
+            <a href='https://github.com/ARC-Solutions' target='_blank'>
+              <p className='text-sm sm:text-lg font-extralight'>ARC-Solutions</p>
+            </a>
+            &nbsp; &nbsp;
+            <FaXTwitter className='w-6 h-6 sm:w-7 sm:h-7  mr-2' />
+            <a href='https://twitter.com/ARCTeamGroup' target='_blank'>
+              <p className='text-sm sm:text-lg font-extralight'>ARCTeamGroup</p>
+            </a>
+          </div>
+          <p className='text-sm sm:text-base lg:text-lg font-extralight pt-3'>
+            {' '}
+            Welcome to our AI quiz platform where your interests take the lead. Explore a wide range
+            of personalized quizzes tailored to your passions, making learning and discovery an
+            engaging and enjoyable experience.
+          </p>
+        </CardHeader>
+        <CardContent className='pb-1'>
+          <div className='border-b-[0.5px] border-primary pt-1 pb-1 opacity-30'></div>
+          <h1 className='py-2 mt-4 text-lg'>Build with</h1>
+
+          <div className='grid grid-cols-2 md:grid-cols-4 gap-6 w-full mt-2 justify-content-between  '>
+            <div className='flex items-center'>
+              <SiTailwindcss className='w-9 h-9 mr-2' />
+
+              <p className='font-extralight text-xl'>Next.js</p>
+            </div>
+            <div className='flex items-center'>
+              <SiTailwindcss className='w-9 h-9 mr-2' />
+              <p className='font-extralight text-xl'>AWS</p>
+            </div>
+            <div className='flex items-center'>
+              <SiTailwindcss className='w-9 h-9 mr-2' />
+              <p className='font-extralight text-lg'>Tailwind</p>
+            </div>
+            <div className='flex items-center'>
+              <SiTailwindcss className='w-9 h-9 mr-2' />
+              <p className='font-extralight text-lg'>OpenAI</p>
+            </div>
+            <div className='flex items-center md:pt-5 '>
+              <SiTailwindcss className='w-9 h-9 mr-2' />
+
+              <p className='font-extralight text-xl'>TypeScript</p>
+            </div>
+            <div className='flex items-center md:pt-5'>
+              <SiTailwindcss className='w-9 h-9 mr-2' />
+              <p className='font-extralight text-xl'>Supabase</p>
+            </div>
+            <div className='flex items-center md:pt-5'>
+              <SiTailwindcss className='w-9 h-9 mr-2' />
+              <p className='font-extralight text-lg'>Shadcn</p>
+            </div>
+            <div className='flex items-center md:pt-5'>
+              <SiTailwindcss className='w-9 h-9 mr-2' />
+              <p className='font-extralight text-lg'>Azure</p>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Modal;
