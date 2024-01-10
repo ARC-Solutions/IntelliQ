1455b139799310e49a212872419203ed25b7bcb1
RickyRaveanu <rickyraveanu02@gmail.com>
2024-01-10 11:18:57 +0100

    Update DashboardPage.tsx

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,59 +1,56 @@
 'use client';
 import QuizMe from '@/components/QuizMe';
 import TopPicks from '@/components/TopPicks';
-import { QuizHistories, useQuiz } from '@/contexts/QuizContext';
+import {QuizHistories, useQuiz} from '@/contexts/QuizContext';
 import LoadingQuestions from './LoadingQuestions';
-import { useEffect } from 'react';
-import { redirect } from 'next/navigation';
+import {useEffect} from 'react';
+import {redirect} from 'next/navigation';
 import QuizHistory from './QuizHistory';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { FaQuestionCircle } from 'react-icons/fa';
-import Modal from "@/components/Modal";
 import InfoDialog from "@/components/Info-Dialog";
-const Dashboard = ({
-  prevQuizzes,
-}: {
-  prevQuizzes: {
-    quizzes: QuizHistories[];
-    totalCount: number;
-  };
-}) => {
-  const { isLoading, fetchingFinished: finished, currentQuiz, dispatch, summaryQuiz } = useQuiz();
-  useEffect(() => {
-    dispatch({ type: 'RESET_SUMMARY_QUIZ' });
-    dispatch({ type: 'STORE_QUIZZES', payload: prevQuizzes.quizzes });
-  }, []);
-  useEffect(() => {
-    if (currentQuiz) {
-      const url = `/quiz/play`;
-      redirect(url);
-    }
-    if (summaryQuiz) {
-      redirect(`/summary/${summaryQuiz.quiz_id}`);
-    }
-  }, [currentQuiz, summaryQuiz]);
 
-  if (isLoading) {
-    return <LoadingQuestions finished={finished} />;
-  }
-  return (
-    <div className='flex flex-col items-center justify-center'>
-      <h1 className='text-3xl lg:text-4xl text-center font-semibold'>DASHBOARD</h1>
-      <InfoDialog />
-      <div className='lg:grid lg:grid-flow-col gap-8 mt-12 m-2'>
-        <div className='col-span-1 m-4 lg:m-0'>
-          <QuizMe />
+const Dashboard = ({
+                       prevQuizzes,
+                   }: {
+    prevQuizzes: {
+        quizzes: QuizHistories[];
+        totalCount: number;
+    };
+}) => {
+    const {isLoading, fetchingFinished: finished, currentQuiz, dispatch, summaryQuiz} = useQuiz();
+    useEffect(() => {
+        dispatch({type: 'RESET_SUMMARY_QUIZ'});
+        dispatch({type: 'STORE_QUIZZES', payload: prevQuizzes.quizzes});
+    }, []);
+    useEffect(() => {
+        if (currentQuiz) {
+            const url = `/quiz/play`;
+            redirect(url);
+        }
+        if (summaryQuiz) {
+            redirect(`/summary/${summaryQuiz.quiz_id}`);
+        }
+    }, [currentQuiz, summaryQuiz]);
+
+    if (isLoading) {
+        return <LoadingQuestions finished={finished}/>;
+    }
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            <h1 className='text-3xl lg:text-4xl text-center font-semibold'>DASHBOARD</h1>
+            <InfoDialog/>
+            <div className='lg:grid lg:grid-flow-col gap-8 mt-12 m-2'>
+                <div className='col-span-1 m-4 lg:m-0'>
+                    <QuizMe/>
+                </div>
+                <div className='row-span-2 mt-10 col-span-1 m-4 lg:m-0'>
+                    <TopPicks/>
+                </div>
+                <div className='row-span-3 mt-10 lg:mt-0 m-4 lg:m-0'>
+                    <QuizHistory totalQuiz={prevQuizzes.totalCount}/>
+                </div>
+            </div>
         </div>
-        <div className='row-span-2 mt-10 col-span-1 m-4 lg:m-0'>
-          <TopPicks />
-        </div>
-        <div className='row-span-3 mt-10 lg:mt-0 m-4 lg:m-0'>
-          <QuizHistory totalQuiz={prevQuizzes.totalCount} />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Dashboard;
